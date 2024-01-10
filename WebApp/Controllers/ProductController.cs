c06cef43a9afc1d3fe808062ec596c34308acc05
Greg Osetrov <shadow-greg@yandex.ru>
2024-01-10 14:12:55 +0300

    не корректно работает сохрание в БД

--- WebApp/Controllers/ProductController.cs
+++ WebApp/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Core.Models;
 using DataBase.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers;
 
@@ -17,6 +19,35 @@ public class ProductController: ControllerBase {
                     Description = x.Description
                 }).ToList();
                 return Ok(products);
+            }
+        }
+        catch (Exception e) {
+            Console.WriteLine(e);
+            return StatusCode(500);
+        }
+    }
+
+    [HttpPost("Add product to data base")]
+    public IActionResult PutProduct(ProductResponse productResponse) {
+        try {
+            using (var context = new DataContext()) {
+                if (!context.Products.Any().Equals(productResponse.Name)) {
+                    context.Products.Add(new Product() {
+                        Id = productResponse.Id,
+                        Name = productResponse.Name,
+                        Description = productResponse.Description,
+                        Cost = 10,
+                        CategoryId = 0,
+                        Category = null,
+                    });
+                    context.SaveChanges();
+                    var id = context.Products
+                        .First(p => EF.Functions.Like(p.Name, productResponse.Name))
+                        .Id;
+                    return Ok(id);
+                }
+
+                return StatusCode(409);
             }
         }
         catch (Exception e) {
---
+++ WebApp/Models/ProductResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models;
+
+public class ProductResponse {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+}
