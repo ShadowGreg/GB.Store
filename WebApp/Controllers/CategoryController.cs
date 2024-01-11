3f6a50174a0ed951c800596e9a0516217b550f35
Greg Osetrov <shadow-greg@yandex.ru>
2024-01-11 11:37:03 +0300

    Работает удаление категории

---
+++ WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Core.Models;
+using DataBase.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CategoryController: ControllerBase {
+    [HttpDelete("Delete category")]
+    public async Task<IActionResult> DeleteCategory(CategoryResponse categoryResponse) {
+        try {
+            using (var context = new DataContext()) {
+                var temp = context.Categories.Find(categoryResponse.Id);
+                if (temp != null) {
+                    context.Categories.Remove(temp);
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
+        catch (Exception e) {
+            Console.WriteLine(e);
+            return StatusCode(500);
+        }
+
+        return Ok();
+    }
+}
