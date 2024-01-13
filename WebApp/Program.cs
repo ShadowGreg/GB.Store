f8a6cc4754807e8c1101e5ea13990cfbdf037624
Greg Osetrov <shadow-greg@yandex.ru>
2024-01-13 12:56:13 +0300

    работает выгрузка и передача файла на скачивание

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,26 +1,13 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using DataBase;
 using DataBase.Repositories;
 using DataBase.Repositories.Abstraction;
-using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1 {
     public class Program {
         public static void Main(string[] args) {
             var host = CreateHostBuilder(args).Build();
-            // using (var scope = host.Services.CreateScope()) {
-            //     var db = scope.ServiceProvider.GetRequiredService<DataContext>();
-            //     // Check if there are any pending migrations
-            //     if (db.Database.GetPendingMigrations().Any()) {
-            //         // Handle the case when there are pending migrations
-            //         Console.WriteLine(
-            //             "There are pending migrations. Please apply the migrations before running the application.");
-            //     }
-            //     else {
-            //          db.Database.Migrate();
-            //     }
-            // }
+                ///перестала адекватно работаь накатка миграций
 
             host.Run();
         }
